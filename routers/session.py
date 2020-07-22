8e5f9b4607ed01bad1d7b74e82ffbcb2b159a7c2
skywalker29 <nishant@noto.ai>
2020-07-22 19:27:35 +0530

    added api skeletons

--- routers/session.py
+++ routers/session.py
@@ -3,12 +3,31 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/session/{session_id")
+@router.get("/session/{id")
 def get_session():
-    print("Reached")
     pass
 
 
-@router.put("/session/{session_id}")
+@router.put("/session/{id}")
 def update_session():
+    pass
+
+
+@router.post("/session")
+def create_session():
+    pass
+
+
+@router.get("/session/{id}/attendance")
+def get_session_attendance():
+    pass
+
+
+@router.get("/session/{id}/pulse")
+def get_session_pulse():
+    pass
+
+
+@router.get("/session/{id}/student_pulse")
+def get_session_student_pulse():
     pass
